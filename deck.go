9870914b3c27871a55d32705f5f89d4af782fd68
cristian <cristian.ortiz@uda.cl>
2021-04-06 00:01:00 -0400

    save a deck type into a file function, and also read a string file and convert it into a deck type and print ir in console, captures read errors too

--- deck.go
+++ deck.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 )
 
 //create a new type 'deck' a slice of strings, in this new type we use and expands strings type behavior
 type deck []string
 
-//creates a new deck of 52 cards
+//creates a new deck of  cards
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
-	//iterates the slices and make a new deck of cards
+	//iterates the slices and make a new deck of cards _index not used in code
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
@@ -34,7 +36,6 @@ func (d deck) print() {
 func deal(d deck, handSize int) (deck, deck) {
 	//split original d deck, in one deck of cards from index 0 to handSize and other from index handSize to end of the slice
 	return d[:handSize], d[handSize:]
-
 }
 
 //reciever to convert a deck type into string
@@ -42,5 +43,27 @@ func (d deck) toString() string {
 	//d deck to slice of string []string(d)
 	//now the slice of string into a single string comma separated ["a","b"]->("a,b")
 	return strings.Join([]string(d), ",")
+}
+
+//receiver to save a deck of cards into a file, te function return an error if thats happends
+func (d deck) saveToFile(filename string) error {
+	//convert the d from single string and then convert to []byte
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+//reads a plain string file and convert to deck type
+func newDeckFromFile(filename string) deck {
+	//ReadFile returns a byteSlice from the file, err is nil at least and error is returned
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil { //error handling in Go
+		//1. -log the error and return a call to a newDeck()
+		//2. -Log the error and entirely quit the program
+		fmt.Println("!Error:", err) //log the error
+		os.Exit(1)                  //exit program on error
+	}
+	//convert the byteSlice, for ex;[50,60,100] into a plain string ("Ace of Spades,Two of Spades")
+	s := strings.Split(string(bs), ",") //then split to convert in format ("Ace of Spades","Two of Spades")etc
+	//convert the string splited into deck (remember deck is string based type already)
+	return deck(s)
 
 }
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 	//creates a new decks of cards
-	cards := newDeck()
+	//cards := newDeck()
 	//split the deck of cards in two hands of fixed size
 	//hand, remainingCards := deal(cards, 5)
 	//print the cards in both hands
-	fmt.Println(cards.toString())
+	//cards.saveToFile("my_deck")
+	cards := newDeckFromFile("my_dec")
+	cards.print()
 
 }
